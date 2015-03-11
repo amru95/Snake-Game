8ac440e8eae6603fab2bcdf08cb952eb7125e3c7
Amr Fathy <amru_fathy@yahoo.com>
2015-03-11 20:11:31 +0200

    Update Brick.h

--- Brick.h
+++ Brick.h
@@ -5,25 +5,10 @@
 class Brick : public Coordinate2D
 {
     public:
-        void Draw(){
-            COORD coord;
-            coord.X = this->x;
-            coord.Y = this->y;
-            SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord) ;
-            std :: cout << char(177);
-        }
-        void setPosition(int x1, int y1){
-            this->x = x1;
-            this->y = y1;
-        }
-        void setPosition(Brick position){
-            this->x = position.x;
-            this->y = position.y;
-        }
-        void setPosition(Coordinate2D position){
-            this->x = position.x;
-            this->y = position.y;
-        }
+        void Draw();
+        void setPosition(int, int);
+        void setPosition(Brick);
+        void setPosition(Coordinate2D);
 
 friend class Wall;
 };
