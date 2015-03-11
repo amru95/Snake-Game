5cb33a9cd6d963bc316f118e2ec6a3aa5e4990be
Amr Fathy <amru_fathy@yahoo.com>
2015-03-11 20:15:58 +0200

    Update Snake.h

--- Snake.h
+++ Snake.h
@@ -10,36 +10,14 @@ class Snake
 
     public:
         Snake();
-        enum  Directions {Left, Right, Up, Down} SnakeDirection;
-        void intalize(int length=3){
-            Length=length;
-            SnakeDirection = Right;
-            Body = new Brick[1000];
-            Body[0].setPosition(39,12);
-            for(int i=1;i<Length;i++)
-                Body[i].setPosition(Body[i-1].x -1, Body[i-1].y);
-        }
-        void Move(){
-            for(int i=Length-1;i>0;i--)
-                Body[i].setPosition(Body[i-1]);
-            if(SnakeDirection == Right)
-                Body[0].setPosition(Body[0].x + 1, Body[0].y);
-            else if(SnakeDirection == Left)
-                Body[0].setPosition(Body[0].x -1 , Body[0].y);
-            else if(SnakeDirection == Up)
-                Body[0].setPosition(Body[0].x , Body[0].y - 1);
-            else
-                Body[0].setPosition(Body[0].x , Body[0].y + 1);
-        }
-        void Draw(){
-            for(int i=0;i<Length;i++)
-                Body[i].Draw();
-        }
+        enum  Directions { Left, Right, Up, Down} SnakeDirection;
+        void intalize(int);
+        void Move();
+        void Draw();
         void keyboardControl();
 
 friend class Food;
 friend class KeyboardControl;
-
 friend class Game;
 };
 
