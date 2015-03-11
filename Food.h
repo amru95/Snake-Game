d9edb241264eae903ba8fe21f544acf472b50202
Amr Fathy <amru_fathy@yahoo.com>
2015-03-11 20:12:39 +0200

    Update Food.h

--- Food.h
+++ Food.h
@@ -7,35 +7,10 @@ class Food
         Brick apple;
 
     public:
-        int random(int min, int max){
-            static bool first = true;
-            if ( first ){
-                srand(time(NULL));
-                first = false;
-            }
-            return min + rand() % (max - min);
-        }
-        bool OccupiedPlace(Snake s, Coordinate2D suggestedPlace ){
-            for(int i=0;i<s.Length;i++)
-                if(s.Body[i] == suggestedPlace)
-                    return true;
-            return false;
-        }
-        void Intalize(const Snake& snake){
-            int GetXposition,GetYposition;
-            Coordinate2D suggestedPoint;
-            do{
-                GetXposition = random(2,77);
-                GetYposition = random(2,22);
-                suggestedPoint.set(GetXposition,GetYposition);
-            }
-            while(OccupiedPlace(snake,suggestedPoint));
-
-            apple.setPosition(suggestedPoint);
-        }
-        void Draw(){
-            apple.Draw();
-        }
+        int random(int min, int max);
+        bool OccupiedPlace(Snake s, Coordinate2D suggestedPlace );
+        void Intalize(const Snake& snake);
+        void Draw();
 
 friend class Game;
 };
