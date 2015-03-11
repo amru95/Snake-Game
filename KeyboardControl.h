9f90a169509a81e986285245f809e2d7d32bb6af
Amr Fathy <amru_fathy@yahoo.com>
2015-03-11 20:13:57 +0200

    Update KeyboardControl.h

--- KeyboardControl.h
+++ KeyboardControl.h
@@ -1,6 +1,7 @@
 #ifndef KEYBOARDCONTROL_H
 #define KEYBOARDCONTROL_H
 
+
 class KeyboardControl
 {
     #define KB_UP 72
@@ -10,36 +11,7 @@ class KeyboardControl
     #define KB_ESCAPE 27
     #define KB_ENTER 13
     public:
-        void update(Snake& s){
-            char key = _getch();
-            if (key== -32)
-            {
-                key=_getch();
-                switch(key){
-                    case KB_UP:
-                        if(s.SnakeDirection != s.Down )
-                            s.SnakeDirection = s.Up;
-                        break;
-                    case KB_DOWN:
-                        if(s.SnakeDirection != s.Up)
-                            s.SnakeDirection = s.Down;
-                        break;
-                    case KB_RIGHT:
-                        if(s.SnakeDirection != s.Left)
-                            s.SnakeDirection = s.Right;
-                        break;
-                    case KB_LEFT:
-                        if(s.SnakeDirection != s.Right)
-                            s.SnakeDirection = s.Left;
-                        break;
-                    }
-            }
-            else if(key ==  KB_ESCAPE)
-                exit(0);
-
-            else if(key == KB_ENTER)
-                while(_getch() != KB_ENTER){}  // pause
-        }
+        void update(Snake& s);
 };
 
 #endif // KEYBOARDCONTROL_H
