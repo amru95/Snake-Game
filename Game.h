38e5e66fc47709616078bbc0b389ac1358c3331b
Amr Fathy <amru_fathy@yahoo.com>
2015-03-11 20:13:27 +0200

    Update Game.h

--- Game.h
+++ Game.h
@@ -18,55 +18,12 @@ class Game
 
     public:
         Game(int , int , int, int);
-        void Draw(){
-            system("CLS");
-            snake.Draw();
-            food.Draw();
-            wall.Draw();
-        }
-        bool hitfood(Snake& s, Food& f){
-            return (s.Body[0] == f.apple);
-        }
-        bool hitItself(Snake& s){
-            for(int i=1;i<s.Length;i++)
-                if(s.Body[0] == s.Body[i] )
-                    return true;
-            return false;
-        }
-        bool hitItwall(Snake& s, Wall& w){
-            for(int i=0;i<w.Maplength;i++)
-                if(s.Body[0] == w.Map[i] )
-                    return true;
-            return false;
-        }
-        void GameLogic(Snake& s, Food& f, Wall& w){
-            if(hitfood(s,f)){
-                s.Length++;
-                int dirx,diry;
-                dirx = diry = 0;
-                s.Move();
-                s.Draw();
-                f.Intalize(s);
-                score += 1;
-            }
-            else if ( hitItself(s) || hitItwall(s,w) ){
-                Game game(Level, --PlayerLives,SnakeLength, score);
-                game.Update();
-            }
-        }
-        void Update(){
-            while(PlayerLives > 0){
-                if(kbhit())
-                    kbControl.update(snake);
-                snake.Move();
-                GameLogic(snake, food, wall);
-                Draw();
-                printf("\nScore: %d",score);
-                Sleep(Level);
-            }
-            exit(0);
-        }
-
+        void Draw();
+        bool hitfood(Snake& s, Food& f);
+        bool hitItself(Snake& s);
+        bool hitItwall(Snake& s, Wall& w);
+        void GameLogic(Snake& s, Food& f, Wall& w);
+        void Update();
 };
 
 #endif // GAME_H
